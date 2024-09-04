f940c757498d706dbfe6c3692c8aa219ade8b0cf
kvba <132848447+imkvba@users.noreply.github.com>
2024-09-04 16:04:56 +0200

    Update server.lua

--- esx_gunshop/server.lua
+++ esx_gunshop/server.lua
@@ -1,6 +1,6 @@
 ESX = nil
 
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+ESX = exports["es_extended"]:getSharedObject()
 
 RegisterNetEvent('esx_gunshop:buyFullKit')
 AddEventHandler('esx_gunshop:buyFullKit', function(weapon)
